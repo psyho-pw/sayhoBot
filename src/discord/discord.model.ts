51cb2cabcd021afd74c16f4177bdbd949fa4c321
sayho <fishcreek@naver.com>
2023-11-19 19:37:55 +0900

    Refactor/class type (#46)
    * feat: add types for simple-youtube-api and assert instance
    
    * feat: replace deprecated type

---
+++ src/discord/discord.model.ts
@@ -0,0 +1,36 @@
+import {StageChannel, VoiceChannel} from 'discord.js'
+
+export class Song {
+    duration: string | null
+
+    thumbnail: string
+
+    title: string
+
+    url: string
+
+    video: any
+
+    videoId: string
+
+    voiceChannel: VoiceChannel | StageChannel
+}
+
+export class ParsedPlayCommand {
+    content: string
+
+    voiceChannel: VoiceChannel | StageChannel
+
+    constructor(content: string, voiceChannel: VoiceChannel | StageChannel) {
+        this.content = content
+        this.voiceChannel = voiceChannel
+    }
+}
+
+export class SelectListItem {
+    label: string
+
+    description: string
+
+    value: string
+}
